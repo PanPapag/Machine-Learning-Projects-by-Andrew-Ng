016000c2efe73b9b9bafa9da51e94b730158f61b
Pantelis Papageorgiou <pan_papageorgiou@hotmail.com>
2019-07-03 23:22:16 +0300

    Update featureNormalize.m

--- ex1/featureNormalize.m
+++ ex1/featureNormalize.m
@@ -26,12 +26,9 @@ sigma = zeros(1, size(X, 2));
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
 
-
-
-
-
-
-
+mu = mean(X);
+sigma = std(X);
+X_norm = (X - mu)./ sigma
 
 
 % ============================================================

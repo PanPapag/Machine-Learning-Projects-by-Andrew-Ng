7bb5d918d32947c6daa2a67c46d8654a18a8aa8c
Pantelis Papageorgiou <pan_papageorgiou@hotmail.com>
2019-07-03 23:35:12 +0300

    Update gradientDescentMulti.m

--- ex1/gradientDescentMulti.m
+++ ex1/gradientDescentMulti.m
@@ -6,6 +6,7 @@ function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
+features = size(X,2);
 
 for iter = 1:num_iters
 
@@ -16,16 +17,11 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-
-
-
-
-
-
-
-
-
+    temp = zeros(features,1);
+    for j = 1:features
+        temp(j) = theta(j) - alpha * (1 / m) * sum(((X * theta) - y).* X(:,j));
+    end 
+    theta = temp;
 
     % ============================================================
 

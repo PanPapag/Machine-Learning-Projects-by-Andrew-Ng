bf5183732b0534d0a1bd48e8bdf4784596ec1480
Pantelis Papageorgiou <pan_papageorgiou@hotmail.com>
2019-07-04 13:41:38 +0300

    Update costFunctionReg.m

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -18,8 +18,11 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
+tempTheta = theta;
+tempTheta(1) = 0;
 
-
+J = - (1 / m) * sum(y.* log(sigmoid(X * theta)) + (1 - y).* log(1 - (sigmoid(X * theta)))) + (lambda / (2 * m)) * sum(tempTheta.^2)
+grad = (1 / m ) * X' * (sigmoid(X * theta) - y) + (lambda / m) * tempTheta
 
 
 % =============================================================

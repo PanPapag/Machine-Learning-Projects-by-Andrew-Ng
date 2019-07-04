3db8e3bd534cafe7c0ac81af1094ee8b72f0984c
Pantelis Papageorgiou <pan_papageorgiou@hotmail.com>
2019-07-04 13:28:46 +0300

    Update costFunction.m

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -21,9 +21,8 @@ grad = zeros(size(theta));
 %
 
 
-
-
-
+J = - (1 / m) * sum(y.* log(sigmoid(X * theta)) + (1 - y).* log(1 - (sigmoid(X * theta))))
+grad = (1 / m ) * X' * (sigmoid(X * theta) - y)
 
 
 

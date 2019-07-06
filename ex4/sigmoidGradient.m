12e635c5a29539faa4d4c74073cfa3d3ae158276
Pantelis Papageorgiou <pan_papageorgiou@hotmail.com>
2019-07-06 23:49:25 +0300

    Update sigmoidGradient.m

--- ex4/sigmoidGradient.m
+++ ex4/sigmoidGradient.m
@@ -13,16 +13,7 @@ g = zeros(size(z));
 %               each value of z (z can be a matrix, vector or scalar).
 
 
-
-
-
-
-
-
-
-
-
-
+g = sigmoid(z).* sigmoid(1 - z);
 
 
 % =============================================================
